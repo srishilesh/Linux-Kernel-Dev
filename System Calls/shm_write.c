eec4f5e094457734929332da3ed6ee33b60d9925
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-01-24 21:28:58 +0530

    Shared Memory

---
+++ System Calls/shm_write.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/ipc.h>
+#include<sys/shm.h>
+#include<sys/types.h>
+int main()
+{
+int shmid,retval;
+char *memory=NULL;
+char *p;
+key_t key=1023;
+shmid=shmget(key,5,IPC_CREAT | 0666);
+printf("shmid:%d\n",shmid);
+memory=shmat(shmid,NULL,0);
+if(memory==NULL)
+{
+printf("Not attached\n");
+return 0;
+}
+p=(char*)memory;
+strcpy(p,"Hello");
+retval=shmdt(p);
+if(retval<0)
+{
+printf("Detachment failed\n");
+return 0;
+}
+return 0;
+}
+
+
