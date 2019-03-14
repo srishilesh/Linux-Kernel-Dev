c66e479c838bb30cc0110f45d033f0b84be6882f
Srishilesh P S <32015336+srishilesh@users.noreply.github.com>
2019-03-14 22:22:56 +0530

    Commented

--- Memory Management/Paging/FIFO.java
+++ Memory Management/Paging/FIFO.java
@@ -6,17 +6,19 @@ public class fifo1
     public static void main(String args[])
     {
         Scanner sc=new Scanner(System.in);
-        int blk=3;
+        int blk;
+        System.out.println("Enter the number of block: ");
+        blk=sc.nextInt();   // Accepting Block size
         int alloc[]=new int[blk];
         System.out.println("Enter the number of Requests:");
-        int n=sc.nextInt();
+        int n=sc.nextInt();     // Accepting the number of Requests
         System.out.println("Enter the requests: ");
         int req[]=new int[n];
         for(int i=0;i<n;i++)
-            req[i]=sc.nextInt();
+            req[i]=sc.nextInt();        // Accepting the requests
         int page=0;int found=-1,x=0;
         for(int i=0;i<blk;i++)
-            alloc[i]=-1;
+            alloc[i]=-1;        // Initializing Alloc to -1
         for(int i=0;i<n;i++)
         {
 
@@ -24,20 +26,20 @@ public class fifo1
             {
                 //if(found==-1)
                 
-                if(alloc[x]!=req[i])
+                if(alloc[x]!=req[i])    // Check repetition of values
                 {
-                    alloc[x]=req[i];
-                    page=page+1;
+                    alloc[x]=req[i];    // Does Allocation
+                    page=page+1;        // Increment Page fault
                 }
                 
-                x=(x+1)%3;
+                x=(x+1)%3;      // Works as Circular Queue to switch between blocks
 
             }
             
 
             
         }
-        for(int i=0;i<blk;i++)
+        for(int i=0;i<blk;i++)      // Printing data
             System.out.print(alloc[i]+" ");
         System.out.println("Number of Page Faults is "+page);
         System.out.println("Percentage of page Faults are: "+(page/n)*100);
